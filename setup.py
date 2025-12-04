b1ba7914662d1872fbbe60ddabe324dd5fe63e71
Thiksha14 <suruthikshas14@gmail.com>
2025-12-04 14:08:38 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Suruthiksha Srinivasan",
+    author_email="suruthikshas14@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
